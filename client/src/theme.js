b0b05cdc83bf7a1c13b4dd3c5949da2220208b1a
jayeshbhole7 <jayubhole7@gmail.com>
2023-08-24 21:33:49 +0530

    Deployed website

--- client/src/theme.js
+++ client/src/theme.js
@@ -104,3 +104,10 @@ export const colorTokens = {
       },
     };
   };
+
+
+  // export const WidgetWrapper2 = styled (Box)(({ theme }) => ({
+  //   padding: "1.5rem 1.5rem 0.75rem 1.5rem",
+  //   backgroundColor: theme.palette.background.alt,
+  //   borderRadius: "0.75rem",
+  // }));
