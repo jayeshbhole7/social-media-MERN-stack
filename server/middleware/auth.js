1fda0a0458e2c6614a28d3c4a8b0f1194d76ac8d
jayeshbhole7 <jayubhole7@gmail.com>
2023-12-26 23:35:57 +0530

    Enhance route protection for authenticated users in App component

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,4 @@
 import  jwt  from "jsonwebtoken";
-
 export const verifyToken = async (req, res, next)=>{
     try{
         let token = req.header("Authorization");
@@ -7,11 +6,9 @@ export const verifyToken = async (req, res, next)=>{
         if(!token){
             return res.status(403).send("Access Denied");
         }
-
         if(token.startsWith("Bearer")){
             token = token.slice(7,token.length).trimLeft();
         }
-
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
         next();
